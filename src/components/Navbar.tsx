7345112bb816a3b8a2c03340099a42fcd364a673
ky-shinsoft <mirudapon@gmail.com>
2026-06-08 15:01:23 +0800

    feat: styling default layout and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,11 +12,14 @@ interface Props {
 
 function Navbar({ items }: Props) {
   return (
-    <nav>
-      <ul>
+    <nav className="pt-[32px] w-[250px]">
+      <ul className="flex flex-col text-xl">
         {items.map(item => (
           <li key={item.label}>
-            <a href={item.to} key={item.to}>
+            <a
+              className="flex items-center gap-8 p-[16px] hover:bg-gray-100 rounded-[9999px]"
+              href={item.to} key={item.to}
+            >
               <item.icon size={24} />
               {item.label}
             </a>
