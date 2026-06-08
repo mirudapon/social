f246a792feff76c15271865cc233380c0df4fed8
ky-shinsoft <mirudapon@gmail.com>
2026-06-08 11:26:01 +0800

    chore: add ts and eslint

---
+++ astro.config.ts
@@ -0,0 +1,9 @@
+import react from '@astrojs/react'
+
+// @ts-check
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+})
